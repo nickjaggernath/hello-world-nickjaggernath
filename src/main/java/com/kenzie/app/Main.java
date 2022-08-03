1af590064035e2093adfe069d42c8e36750cafec
nickjaggernath <110433968+nickjaggernath@users.noreply.github.com>
2022-08-02 22:00:17 -0400

    Name

--- src/main/java/com/kenzie/app/Main.java
+++ src/main/java/com/kenzie/app/Main.java
@@ -3,7 +3,7 @@ package com.kenzie.app;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        // Edit the following line and put in your name
+        // edit the following line and put in your name
         System.out.println("My name is Nicklas Jaggernath");
     }
 }
